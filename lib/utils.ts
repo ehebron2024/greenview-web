547d7164b394e8a06ec6b1cd16b825ff6860e252
ehebron2024 <ehebron2024@fau.edu>
2025-11-21 01:57:59 -0500

    commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return clsx(inputs); // Use clsx directly to combine class names
 }
+  
