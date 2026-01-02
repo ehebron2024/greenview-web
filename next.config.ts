1360a357eeee12d67791ae50670389d9e8a97f08
ehebron2024 <ehebron2024@fau.edu>
2026-01-02 00:29:54 -0500

    save

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Generates the 'out' folder instead of '.next'
-  basePath: '/greenview-web', // MUST match your GitHub repository name
   images: {
-    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+    unoptimized: true,
   },
 };
 
